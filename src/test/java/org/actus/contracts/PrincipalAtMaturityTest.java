b45742b13026a3ed97b3943b0837529c3dc1ae4a
nbundi <nils.bundi@gmail.com>
2016-08-18 02:56:38 +0000

    added another junit test method for PrincipalAtMaturity with multiple analysis times as input

--- src/test/java/org/actus/contracts/PrincipalAtMaturityTest.java
+++ src/test/java/org/actus/contracts/PrincipalAtMaturityTest.java
@@ -403,58 +403,34 @@ public class PrincipalAtMaturityTest {
         // eval PAM contract
         ArrayList<ContractEvent> events = contract.eval(riskFactors);
     }
-
-    @Test
-    public void test_PAM_eval_AllAttributes() {
+    
+        @Test
+    public void test_PAM_eval_withIP_withRR_withSC_withOP_withMultipleAnalysisTimes() {
         thrown = ExpectedException.none();
         Map<String, String> map = new HashMap<String, String>();
         map.put("ContractType", "PAM");
-        map.put("Calendar", "org.actus.time.calendar.NoHolidaysCalendar");
-        map.put("BusinessDayConvention", "SCF");
-        map.put("EndOfMonthConvention", "SD");
+        map.put("Calendar", "NoHolidayCalendar");
         map.put("StatusDate", "2016-01-01T00:00:00");
         map.put("ContractRole", "RPA");
         map.put("LegalEntityIDCounterparty", "CORP-XY");
-        map.put("CycleAnchorDateOfFee", "2016-01-02T00:00:00");
-        map.put("CycleOfFee", "1Q-");
-        map.put("FeeBasis", "1000.0");
-        map.put("FeeRate", "0.05");
-        map.put("FeeAccrued", "0.0");
-        map.put("CycleAnchorDateOfInterestPayment", "2016-01-02T00:00:00");
-        map.put("CycleOfInterestPayment", "1M+");
-        map.put("NominalInterestRate", "0.01");
         map.put("DayCountConvention", "A/AISDA");
-        map.put("AccruedInterest", "0.0");
-        map.put("CapitalizationEndDate", "2016-04-02T00:00:00");
-        map.put("CyclePointOfInterestPayment", "END");
         map.put("Currency", "USD");
         map.put("InitialExchangeDate", "2016-01-02T00:00:00");
-        map.put("PremiumDiscountAtIED", "-100.0");
         map.put("MaturityDate", "2017-01-01T00:00:00");
         map.put("NotionalPrincipal", "1000.0");
-        map.put("PurchaseDate", "2016-05-01T00:00:00");
-        map.put("PriceAtPurchaseDate", "800.0");
-        map.put("TerminationDate", "2016-07-01T00:00:00");
-        map.put("PriceAtTerminationDate", "900.0");
-        map.put("MarketObjectCodeOfScalingIndex", "Index-XY");
-        map.put("ScalingIndexAtStatusDate", "1000.0");
-        map.put("CycleAnchorDateOfScalingIndex", "2016-01-02T00:00:00");
-        map.put("CycleOfScalingIndex", "6M-");
-        map.put("ScalingEffect", "INM");
-        map.put("CycleAnchorDateOfRateReset", "2016-04-02T00:00:00");
-        map.put("CycleOfRateReset", "2M-");
-        map.put("RateSpread", "0.05");
-        map.put("MarketObjectCodeRateReset", "ReferenceRate-XY");
-        map.put("CyclePointOfRateReset", "BEG");
-        map.put("FixingDays", "2D");
-        map.put("NextResetRate", "0.08");
-        map.put("RateMultiplier", "1.1");
-        map.put("RateTerm", "4M");
+        map.put("NominalInterestRate","0.01");
+        map.put("CycleOfInterestPayment","1Q-");
+        map.put("CycleOfRateReset","1Q-");
+        map.put("CycleOfScalingIndex","1Q-");
+        map.put("CycleAnchorDateOfOptionality","2016-06-01T00:00:00");
         // parse attributes
         ContractModel model = AttributeParser.parse(map);
         // define analysis times
         Set<LocalDateTime> analysisTimes = new HashSet<LocalDateTime>();
         analysisTimes.add(LocalDateTime.parse("2016-01-01T00:00:00"));
+        analysisTimes.add(LocalDateTime.parse("2016-04-01T00:00:00"));
+        analysisTimes.add(LocalDateTime.parse("2016-07-01T00:00:00"));
+        analysisTimes.add(LocalDateTime.parse("2016-09-01T00:00:00"));
         // init PAM contracts
         PrincipalAtMaturity contract = new PrincipalAtMaturity();
         contract.init(analysisTimes, model);
@@ -463,4 +439,6 @@ public class PrincipalAtMaturityTest {
         // eval PAM contract
         ArrayList<ContractEvent> events = contract.eval(riskFactors);
     }
+
+
 }
