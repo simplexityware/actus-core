2c11247b942ebe79e9355b172c33c0ce1677b3c9
nbundi <nils.bundi@gmail.com>
2017-05-07 11:44:13 +0000

    implemented contract type Cash with junit testing

---
+++ src/test/java/org/actus/contracts/CashTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2016 - present by ACTUS Foundation for Financial Research
+ *
+ * Please see distribution for license.
+ */
+package org.actus.contracts;
+
+import org.actus.attributes.ContractModel;
+import org.actus.events.ContractEvent;
+import org.actus.states.StateSpace;
+import org.actus.externals.ContractModelProvider;
+import org.actus.externals.RiskFactorModelProvider;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+public class CashTest {
+    
+    class MarketModel implements RiskFactorModelProvider {
+        public Set<String> keys() {
+            Set<String> keys = new HashSet<String>();
+            return keys;
+        }
+        
+        public Set<LocalDateTime> times(String id) {
+            Set<LocalDateTime> times = new HashSet<LocalDateTime>();
+            return times;
+        }
+        
+        public double stateAt(String id,LocalDateTime time,StateSpace contractStates,ContractModelProvider contractAttributes) {
+            return 0.0;    
+        }
+    }
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Test
+    public void test_CSH_MandatoryAttributes() {
+        thrown = ExpectedException.none();
+        // define attributes
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("ContractType", "CSH");
+        map.put("StatusDate", "2016-01-01T00:00:00");
+        map.put("ContractRole", "RPA");
+        map.put("Currency", "USD");
+        map.put("NotionalPrincipal", "1000.0");
+        // parse attributes
+        ContractModel model = ContractModel.parse(map);
+        // define analysis times
+        Set<LocalDateTime> analysisTimes = new HashSet<LocalDateTime>();
+        analysisTimes.add(LocalDateTime.parse("2016-01-01T00:00:00"));
+        // define risk factor model
+        MarketModel riskFactors = new MarketModel();
+        // eval PAM contract
+        ArrayList<ContractEvent> events = Cash.eval(analysisTimes,model,riskFactors);
+    }
+    
+    @Test
+    public void test_CSH_withMultipleAnalysisTimes() {
+        thrown = ExpectedException.none();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("ContractType", "CSH");
+        map.put("StatusDate", "2016-01-01T00:00:00");
+        map.put("ContractRole", "RPA");
+        map.put("Currency", "USD");
+        map.put("NotionalPrincipal", "1000.0");
+        // parse attributes
+        ContractModel model = ContractModel.parse(map);
+        // define analysis times
+        Set<LocalDateTime> analysisTimes = new HashSet<LocalDateTime>();
+        analysisTimes.add(LocalDateTime.parse("2016-01-01T00:00:00"));
+        analysisTimes.add(LocalDateTime.parse("2016-04-01T00:00:00"));
+        analysisTimes.add(LocalDateTime.parse("2016-07-01T00:00:00"));
+        analysisTimes.add(LocalDateTime.parse("2016-09-01T00:00:00"));
+        // define risk factor model
+        MarketModel riskFactors = new MarketModel();
+        // eval PAM contract
+        ArrayList<ContractEvent> events = Cash.eval(analysisTimes,model,riskFactors);
+        //System.out.println(events);
+    }
+}
