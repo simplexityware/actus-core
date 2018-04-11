abbe8ba4dccc3fef1ef36730ea8cf9588a370361
Lakshmi Madam <lakshmi.madam@odedtech.com>
2018-04-11 13:12:52 +0530

    ODEDACTUS-29 NAM - fixed PR calculation

--- src/main/java/org/actus/functions/nam/POF_PR_NAM.java
+++ src/main/java/org/actus/functions/nam/POF_PR_NAM.java
@@ -20,6 +20,6 @@ public final class POF_PR_NAM implements PayOffFunction {
     public double eval(LocalDateTime time, StateSpace states, 
     ContractModelProvider model, RiskFactorModelProvider riskFactorModel, DayCountCalculator dayCounter, BusinessDayAdjuster timeAdjuster) {
         return (1 - states.probabilityOfDefault) * states.nominalScalingMultiplier * 
-                ( states.nextPrincipalRedemptionPayment - states.nominalAccrued - dayCounter.dayCountFraction(states.lastEventTime, time)*states.nominalRate*states.interestCalculationBase );
+                ( states.nextPrincipalRedemptionPayment - states.nominalAccrued - states.timeFromLastEvent * states.nominalRate * states.interestCalculationBase );
         }
 }
--- src/main/java/org/actus/functions/nam/STF_PR_NAM.java
+++ src/main/java/org/actus/functions/nam/STF_PR_NAM.java
@@ -21,12 +21,12 @@ public final class STF_PR_NAM implements StateTransitionFunction {
     public double[] eval(LocalDateTime time, StateSpace states, 
     ContractModelProvider model, RiskFactorModelProvider riskFactorModel, DayCountCalculator dayCounter, BusinessDayAdjuster timeAdjuster) {
         double[] postEventStates = new double[8];
-        
+        double principalRedemption =  states.nextPrincipalRedemptionPayment - states.nominalAccrued - states.timeFromLastEvent * states.nominalRate * states.interestCalculationBase;
         // update state space
         states.timeFromLastEvent = dayCounter.dayCountFraction(states.lastEventTime, time);
         states.nominalAccrued += states.nominalRate * states.interestCalculationBase * states.timeFromLastEvent;
         states.feeAccrued += model.<Double>getAs("FeeRate") * states.nominalValue * states.timeFromLastEvent;
-        states.nominalValue -= states.nextPrincipalRedemptionPayment - states.nominalAccrued;
+        states.nominalValue -= principalRedemption - states.nominalAccrued;
         if (!CommonUtils.isNull(model.getAs("InterestCalculationBase")) && model.getAs("InterestCalculationBase").equals("NTL")) {
             states.interestCalculationBase = states.nominalValue;    
         }
