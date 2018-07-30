cab14116ea2ece3fd2fc67c5a95b48f0f8b1a51f
Lakshmi Madam <lakshmi.madam@odedtech.com>
2018-07-30 13:27:55 +0530

    ODEDACTUS-63 show nominal rate at IP event

--- src/main/java/org/actus/functions/clm/STF_IP_CLM.java
+++ src/main/java/org/actus/functions/clm/STF_IP_CLM.java
@@ -27,8 +27,9 @@ public final class STF_IP_CLM implements StateTransitionFunction {
         
         // copy post-event-states
         postEventStates[0] = states.timeFromLastEvent;
-        postEventStates[6] = states.probabilityOfDefault;
         postEventStates[1] = states.nominalValue;
+        postEventStates[3] = states.nominalRate;
+        postEventStates[6] = states.probabilityOfDefault;
         
         // return post-event-states
         return postEventStates;
