718025bf8e713184dc9dab0e581dc9ea975874f7
Ariadnedev <support@ariadne.swiss>
2018-09-04 15:47:39 +0530

    ODEDACTUS-80 SWPPV - fixed NA at PRD and RR events

--- src/main/java/org/actus/functions/swppv/STF_RR_SWPPV.java
+++ src/main/java/org/actus/functions/swppv/STF_RR_SWPPV.java
@@ -32,7 +32,7 @@ public final class STF_RR_SWPPV implements StateTransitionFunction {
         // copy post-event-states
         postEventStates[0] = states.timeFromLastEvent;
         postEventStates[1] = states.nominalValue;
-        postEventStates[2] = states.nominalAccrued;
+        postEventStates[2] = states.nominalAccruedFloat;
         postEventStates[3] = states.nominalRate;
         postEventStates[6] = states.probabilityOfDefault;
         
