f908469d685369c9ee62bf14c071bc050b94036f
Nick Marti <nick_marti@hotmail.com>
2020-04-30 10:47:39 +0200

    Add settlementCurrencyFxRate to CommonUtils

--- src/main/java/org/actus/util/CommonUtils.java
+++ src/main/java/org/actus/util/CommonUtils.java
@@ -5,6 +5,12 @@
  */
 package org.actus.util;
 
+import org.actus.attributes.ContractModelProvider;
+import org.actus.externals.RiskFactorModelProvider;
+import org.actus.states.StateSpace;
+
+import java.time.LocalDateTime;
+
 /**
  * Contains general-purpose utilities used throughout the library
  */
@@ -12,5 +18,24 @@ public final class CommonUtils {
     
     public static boolean isNull(Object o) {
         return (o == null || String.valueOf(o).equals("NULL"))? true : false;
+    }
+
+    /**
+     * Calculate the Settlement Currency Rate
+     * <p>
+     * @param riskFactorModel an external market model
+     * @param model the model carrying the contract attributes
+     * @param state the inner states of the contract at time
+     * @param time the time at which to observe the fx-rate
+     * @return the fx-rate
+     */
+    public static double settlementCurrencyFxRate(RiskFactorModelProvider riskFactorModel, ContractModelProvider model, LocalDateTime time, StateSpace state){
+        String settlementCurrency = model.getAs("SettlementCurrency");
+        String currency = model.getAs("Currency");
+        if(isNull(settlementCurrency)  || currency.equals(settlementCurrency)){
+            return 1;
+        }else {
+            return riskFactorModel.stateAt(currency + "/" + settlementCurrency, time, state, model);
+        }
     }
 }
