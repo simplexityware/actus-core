e735aa01159c51b52f98a71daf02608c95ea8e7b
Nick <nick_marti@hotmail.com>
2020-07-01 17:54:14 +0200

    Add enum types

---
+++ src/main/java/org/actus/types/BusinessDayConvention.java
@@ -0,0 +1,13 @@
+package org.actus.types;
+
+public enum BusinessDayConvention {
+    NOS,
+    SCF,
+    SCMF,
+    CFS,
+    CSMF,
+    SCP,
+    SCMP,
+    CSP,
+    CSMP
+}
---
+++ src/main/java/org/actus/types/EventType.java
@@ -0,0 +1,26 @@
+package org.actus.types;
+
+public enum EventType {
+    AD,
+    IED,
+    FP,
+    PR,
+    PD,
+    PRF,
+    PY,
+    PP,
+    IP,
+    IPCI,
+    CE,
+    RRF,
+    RR,
+    DV,
+    PRD,
+    MR,
+    TD,
+    SC,
+    IPCB,
+    MD,
+    XD,
+    STD
+}
