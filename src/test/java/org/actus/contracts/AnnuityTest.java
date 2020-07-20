a8cc43dee7418c32cbfe5b26046c63716e319fff
Nick <nick_marti@hotmail.com>
2020-07-20 04:36:20 +0200

    Refactor to use correct Contract-Classes

--- src/test/java/org/actus/contracts/AnnuityTest.java
+++ src/test/java/org/actus/contracts/AnnuityTest.java
@@ -49,8 +49,8 @@ public class AnnuityTest {
             ContractModel terms = ContractTestUtils.createModel(tests.get(testId).getTerms());
 
             // compute and evaluate schedule
-            ArrayList<ContractEvent> schedule = PrincipalAtMaturity.schedule(terms.getAs("MaturityDate"), terms);
-            schedule = PrincipalAtMaturity.apply(schedule, terms, observer);
+            ArrayList<ContractEvent> schedule = Annuity.schedule(terms.getAs("MaturityDate"), terms);
+            schedule = Annuity.apply(schedule, terms, observer);
         
             // transform schedule to event list and return
             List<ResultSet> computedResults = schedule.stream().map(e -> { 
