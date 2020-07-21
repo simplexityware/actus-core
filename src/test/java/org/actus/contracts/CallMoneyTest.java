a21988993f7c09e454e53a082e45cde2042183ba
Nick <nick_marti@hotmail.com>
2020-07-22 01:25:34 +0200

    Use correct Contract-class

--- src/test/java/org/actus/contracts/CallMoneyTest.java
+++ src/test/java/org/actus/contracts/CallMoneyTest.java
@@ -49,8 +49,8 @@ public class CallMoneyTest {
             ContractModel terms = ContractTestUtils.createModel(tests.get(testId).getTerms());
 
             // compute and evaluate schedule
-            ArrayList<ContractEvent> schedule = PrincipalAtMaturity.schedule(terms.getAs("MaturityDate"), terms);
-            schedule = PrincipalAtMaturity.apply(schedule, terms, observer);
+            ArrayList<ContractEvent> schedule = CallMoney.schedule(terms.getAs("MaturityDate"), terms);
+            schedule = CallMoney.apply(schedule, terms, observer);
         
             // transform schedule to event list and return
             List<ResultSet> computedResults = schedule.stream().map(e -> { 
