0f43ea93a9ae906bdb21e50341dcde9b6b1b38e4
Nick <nick_marti@hotmail.com>
2020-08-09 15:32:12 +0200

    Refactor STF_RR_LAM nominalInterestRate calc.

--- src/main/java/org/actus/functions/lam/STF_RR_LAM.java
+++ src/main/java/org/actus/functions/lam/STF_RR_LAM.java
@@ -19,26 +19,21 @@ public final class STF_RR_LAM implements StateTransitionFunction {
     @Override
     public StateSpace eval(LocalDateTime time, StateSpace states,
     ContractModelProvider model, RiskFactorModelProvider riskFactorModel, DayCountCalculator dayCounter, BusinessDayAdjuster timeAdjuster) {
-        // compute new rate
-        double rate = riskFactorModel.stateAt(model.getAs("MarketObjectCodeOfRateReset"), time, states, model)
-                * model.<Double>getAs("RateMultiplier") + model.<Double>getAs("RateSpread");
-        double deltaRate = rate - states.nominalInterestRate;
-
-        // apply period cap/floor
-        deltaRate = Math.min(Math.max(deltaRate,(-1)*model.<Double>getAs("PeriodFloor")),model.<Double>getAs("LifeCap"));
-        rate = states.nominalInterestRate +deltaRate;
-
-        // apply life cap/floor
-        rate = Math.min(Math.max(rate,model.getAs("LifeFloor")),model.getAs("LifeCap"));
-
-
-        // update state space
         double timeFromLastEvent = dayCounter.dayCountFraction(timeAdjuster.shiftCalcTime(states.statusDate), timeAdjuster.shiftCalcTime(time));
+        double rate = (riskFactorModel.stateAt(model.getAs("MarketObjectCodeOfRateReset"), time, states, model)
+                * model.<Double>getAs("RateMultiplier"))
+                + model.<Double>getAs("RateSpread") - states.nominalInterestRate;
+        double deltaRate = Math.min(
+                Math.max(rate, model.<Double>getAs("PeriodFloor"))
+                ,model.<Double>getAs("PeriodCap")
+        );
         states.accruedInterest += states.nominalInterestRate * states.interestCalculationBaseAmount * timeFromLastEvent;
         states.feeAccrued += model.<Double>getAs("FeeRate") * states.notionalPrincipal * timeFromLastEvent;
-        states.nominalInterestRate = rate;
+        states.nominalInterestRate = Math.min(
+                Math.max(states.nominalInterestRate + deltaRate, model.<Double>getAs("LifeFloor"))
+                ,model.<Double>getAs("LifeCap")
+        );
         states.statusDate = time;
-
         // return post-event-states
         return StateSpace.copyStateSpace(states);
     }
