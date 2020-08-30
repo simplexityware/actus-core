c9c5539003fd4caa4751255ce0e9a30baabcca1f
Nick <nick_marti@hotmail.com>
2020-08-30 20:41:59 +0200

    Remove unused imports

--- src/main/java/org/actus/functions/nam/POF_PR_NAM.java
+++ src/main/java/org/actus/functions/nam/POF_PR_NAM.java
@@ -5,14 +5,12 @@
  */
 package org.actus.functions.nam;
 
-import org.actus.conventions.contractrole.ContractRoleConvention;
 import org.actus.functions.PayOffFunction;
 import org.actus.states.StateSpace;
 import org.actus.attributes.ContractModelProvider;
 import org.actus.externals.RiskFactorModelProvider;
 import org.actus.conventions.daycount.DayCountCalculator;
 import org.actus.conventions.businessday.BusinessDayAdjuster;
-import org.actus.types.ContractRole;
 import org.actus.util.CommonUtils;
 
 import java.time.LocalDateTime;
